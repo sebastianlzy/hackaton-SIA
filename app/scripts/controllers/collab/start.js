f425eaaebf0cc5f69f000346d1dd7311882b33a7
ongtingwei <ongtingwei@live.com>
2015-10-04 01:40:31 +0800

    touched start.js and start.html

--- app/scripts/controllers/collab/start.js
+++ app/scripts/controllers/collab/start.js
@@ -18,11 +18,19 @@ angular.module('siahackatonApp')
             'url' : 'http://localhost:9000/#/?name='
         }
 
+        //hey sebas. cant solve the dirty function. TEACH ME!
+        //what i want: to clear the url everytime buddy.name is being changed.
         $scope.$watch('buddy.name', function () {
+            console.log($scope.buddy['name']);
+            if($scope.buddy['name'].$dirty){
+                $scope.buddy['url'] += "";
+                console.log($scope.buddy['name']);
+            }
             $scope.buddy['url'] += $scope.buddy['name'];
         });
 
         $scope.generateBuddyUrl = function(buddy){
+
             $scope.buddy['url'] = 'http://localhost:9000/#/?name=' + buddy.name
         }
 
