40fcc5cfcf13503022f4270cce15140c5c11e531
sebastian <syang@rocketfuel.com>
2015-10-03 17:35:51 +0800

    added material to bower

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -31,6 +31,8 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/bootstrap-material-design/dist/js/material.js',
+      'bower_components/bootstrap-material-design/dist/js/ripples.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
