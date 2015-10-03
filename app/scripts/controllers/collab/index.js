401ecc8a8ca380e4a47e58811334592ee469e81b
sebastian <syang@rocketfuel.com>
2015-10-03 23:20:59 +0800

    added to flybot for KUL

--- app/scripts/controllers/collab/index.js
+++ app/scripts/controllers/collab/index.js
@@ -13,19 +13,38 @@ angular.module('siahackatonApp')
     var ref = new Firebase("https://siahackaton.firebaseio.com/messages");
     $scope.message  = {name : $routeParams['name'] || 'anonymous'};
     $scope.messages = $firebaseArray(ref);
+
+    var checkForKeywords = function (text) {
+      if(text !== ''){
+        if(text.indexOf('KUL') > -1){
+          $timeout(function () {
+            $scope.messages.$add({
+              name : 'Fly',
+              text : '1130 - 1230 flight to KUL at 130 SGD tomorrow'
+            });
+          }, 2000)
+        }
+      }
+    };
     $scope.addMessage = function (message) {
       $scope.messages.$add(angular.copy(message));
+      checkForKeywords(message.text);
+      $scope.message.text = "";
+      scrollToBottom();
     };
 
 
     $scope.messages.$loaded(function () {
-      console.log('hello');
+      scrollToBottom();
+
+    });
+
+    var scrollToBottom = function () {
       $timeout(function () {
         var elem = document.getElementById('message-box');
         elem.scrollTop = elem.scrollHeight;
       });
-
-    });
+    }
 
 
   });
