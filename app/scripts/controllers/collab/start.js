49a4449a3af7441baf48ea8577fb2527027230f7
ongtingwei <ongtingwei@live.com>
2015-10-06 13:30:25 +0800

    added tempName in start.js

--- app/scripts/controllers/collab/start.js
+++ app/scripts/controllers/collab/start.js
@@ -28,12 +28,10 @@ angular.module('siahackatonApp')
         //hey sebas. cant solve the dirty function. TEACH ME!
         //what i want: to clear the url everytime buddy.name is being changed.
         $scope.$watch('buddy.name', function () {
-            console.log($scope.buddy['name']);
-            if($scope.buddy['name'].$dirty){
-                $scope.buddy['url'] += "";
-                console.log($scope.buddy['name']);
-            }
-            $scope.buddy['url'] += $scope.buddy['name'];
+            var tempName=$scope.buddy['name'];
+
+            $scope.buddy['url'] = 'http://localhost:9000/#/?name='
+            $scope.buddy['url'] += tempName;
         });
 
         $scope.generateBuddyUrl = function(buddy){
