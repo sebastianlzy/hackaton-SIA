1dc40332dd1d35fa64315ea1e0f469109cd00351
ongtingwei <ongtingwei@live.com>
2015-10-08 17:55:55 +0800

    changed the generate buddy url to generate own url

--- app/scripts/controllers/collab/start.js
+++ app/scripts/controllers/collab/start.js
@@ -8,7 +8,7 @@
  * Controller of the siahackatonApp
  */
 angular.module('siahackatonApp')
-  .controller('StartCtrl', function ($scope, $location, $http, $timeout) {
+  .controller('StartCtrl', function ($scope, $location, $http, $timeout, $window) {
         console.log($location.url());
 
         $scope.isActive = function (viewLocation) {
@@ -25,18 +25,17 @@ angular.module('siahackatonApp')
             'url' : 'http://localhost:9000/#/?name='
         };
 
-        //hey sebas. cant solve the dirty function. TEACH ME!
-        //what i want: to clear the url everytime buddy.name is being changed.
-        $scope.$watch('buddy.name', function () {
-            var tempName=$scope.buddy['name'];
+        $scope.$watch('buddy.chatOwner', function () {
+            var tempName=$scope.buddy['chatOwner'];
 
-            $scope.buddy['url'] = 'http://www.localhost:9000/#/?name='
+            $scope.buddy['url'] = 'http://www.localhost:9000/#/?name=';
             $scope.buddy['url'] += tempName;
         });
 
         $scope.generateBuddyUrl = function(buddy){
 
-            $scope.buddy['url'] = 'http://www.localhost:9000/#/?name=' + buddy.name
+            $scope.buddy['url'] = 'http://www.localhost:9000/#/?name=' + buddy.chatOwner;
+            $window.open($scope.buddy['url'], '_blank');
         };
 
         $scope.addBuddy = function (buddy) {
@@ -47,11 +46,11 @@ angular.module('siahackatonApp')
             var appID= "app_id=Ts9Wbe2xk2HNFi4f";
             var accessToken= "&access_token=SbRiWZRTw7TVfudb";
             var dest = "&dest=%2B65" + buddy.number;
-            var message= "&msg=Hello%20"+buddy.name+"! Your%20buddy%20has%20invited%20you%20to%20join%20this%20chat: "+buddy.url;
+            var message= "&msg=Hello%20"+buddy.name+"! Your%20buddy%20"+ buddy.chatOwner +"%20has%20invited%20you%20to%20join%20this%20chat: "+buddy.url;
 
             url = url + appID+accessToken+dest+message;
 
-            console.log(dest);
+            console.log(buddy.chatOwner);
             console.log(url);
             $http({
               method: 'GET',
