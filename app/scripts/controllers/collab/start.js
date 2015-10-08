b61a13923c6be614019cc61172e07392f3300ef3
ongtingwei <ongtingwei@live.com>
2015-10-08 18:10:39 +0800

    changed to html url encoding

--- app/scripts/controllers/collab/start.js
+++ app/scripts/controllers/collab/start.js
@@ -46,7 +46,7 @@ angular.module('siahackatonApp')
             var appID= "app_id=Ts9Wbe2xk2HNFi4f";
             var accessToken= "&access_token=SbRiWZRTw7TVfudb";
             var dest = "&dest=%2B65" + buddy.number;
-            var message= "&msg=Hello%20"+buddy.name+"! Your%20buddy%20"+ buddy.chatOwner +"%20has%20invited%20you%20to%20join%20this%20chat: "+buddy.url;
+            var message= "&msg=Hello%20"+buddy.name+"! "+ buddy.chatOwner +"%20has%20invited%20you%20to%20join%20this%20chat:%20http%3A%2F%2Fwww%2Elocalhost%3A9000%2F%23%2F%3Fname%3D"+buddy.name;
 
             url = url + appID+accessToken+dest+message;
 
