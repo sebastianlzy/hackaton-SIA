cf81304d66b62eef28915637daefbf84e36b8ac4
sebastian <syang@rocketfuel.com>
2015-10-10 09:35:28 +0800

    pushing background

--- app/scripts/controllers/collab/index.js
+++ app/scripts/controllers/collab/index.js
@@ -70,7 +70,6 @@ angular.module('siahackatonApp')
     $scope.messages.$loaded(function () {
       scrollToBottom();
       addUserColors();
-      addCssToFlyBot();
     });
 
     $scope.isFlyBot = function(name){
